28fa0ed3f26cde341ed2bf0555b69253b5320305
Agent-003 <abbasova003@gmail.com>
2019-12-21 12:38:39 +0200

    Reverted sass

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\FilmService;
+use App\Services\FilmServiceInterface;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            FilmServiceInterface::class,
+            FilmService::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+    }
+}
